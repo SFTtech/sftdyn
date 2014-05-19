bed575d7a3ac31ad15c3fbda16c722394faff897
Michael Ensslin <michael@ensslin.cc>
2014-05-19 12:21:18 +0200

    implemented call to nsupdate
    not tested yet

--- sftdyn/args.py
+++ sftdyn/args.py
@@ -6,24 +6,33 @@ p.add_argument("-c", "--cert", type=str,
     help="X.509 cert file")
 p.add_argument("-k", "--key", type=str,
     help="X.509 key file")
+p.add_argument("-z", "--zone", type=str,
+    help="DNS target zone")
+p.add_argument("client", type=str, nargs='*',
+    help="clientkey:hostname tuples")
 p.add_argument("-p", "--port", type=int, default=4443)
 p.add_argument("-l", "--listen", type=str, default="0.0.0.0")
 p.add_argument("--conf", type=str,
     help="conf file, will be exec'd as python3, with args as global()")
-p.add_argument("client", type=str, nargs='*',
-    help="tuples of the form clientid:domainname")
+p.add_argument("--nsupdatecommand", type=str,
+    default="zone $ZONE\nupdate delete $HOST A\nadd $HOST 30 A $IP\nsend\n",
+    help="template command list for nsupdate")
 
 def parse():
     args = p.parse_args().__dict__
     globals().update(args)
 
+    global client
     global clients
     clients = {}
     for c in client:
-        ckey, cdns = c.split(':', maxsplit=1)
-        clients[ckey] = cdns
+        ckey, chost = c.split(':', maxsplit=1)
+        clients[ckey] = chost
+    del client
 
     if conf:
         exec(open(conf).read(), globals())
     if not cert or not key:
         p.error("You must provide key and cert files")
+    if not zone:
+        p.error("You must specify the target DNS zone")
--- sftdyn/nsupdater.py
+++ sftdyn/nsupdater.py
@@ -1,4 +1,5 @@
 import sftdyn.args as args
+from subprocess import Popen, PIPE
 
 def handle_request(ckey, ip):
     if ckey not in args.clients:
@@ -6,4 +7,13 @@ def handle_request(ckey, ip):
         return False
 
     print("Updating " + args.clients[ckey] + " to " + ip)
-    return True
+
+    s = args.nsupdatecommand
+    s = s.replace('$HOST', args.clients[ckey])
+    s = s.replace('$IP', ip)
+    s = s.replace('$ZONE', args.zone)
+
+    p = Popen(['nsupdate', '-l'], stdin=PIPE)
+    result = p.communicate(input=s.encode('utf-8'))[0]
+
+    return result is 0
