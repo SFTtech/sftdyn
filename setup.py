129c603de3d05905dbf5ee5a0c39c540288469af
Michael Ensslin <michael@ensslin.cc>
2014-05-20 14:19:57 +0200

    style: made setup.py pep8-compliant

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version=VERSION,
     description="HTTPs-based DDNS updater",
     long_description="dyndns-like service that accepts update requests " +
-        "via HTTPs and forwards them to a locally running DNS " +
-        "server via nsupdate -l",
+                     "via HTTPs and forwards them to a locally running DNS " +
+                     "server via nsupdate -l",
     author="Michael Enßlin",
     author_email="michael@ensslin.cc",
     url="https://github.com/SFTtech/sftdyn",
